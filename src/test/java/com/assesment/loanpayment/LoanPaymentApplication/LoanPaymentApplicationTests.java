91092c1bda2c9794c06ba313b750e7eb6a500c8d
Chris Masina <thato.don@gmail.com>
2025-04-15 07:34:02 -0700

    creating scafolding and configuration / created loan and payment project structre

---
+++ src/test/java/com/assesment/loanpayment/LoanPaymentApplication/LoanPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assesment.loanpayment.LoanPaymentApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
