86ea70fde5403f846175de073a39ed6d77536e78
Chris Masina <thato.don@gmail.com>
2025-04-16 07:28:49 -0700

    clean up and testing

--- src/main/java/com/assesment/loan/service/LoanService.java
+++ src/main/java/com/assesment/loan/service/LoanService.java
@@ -4,20 +4,18 @@ import com.assesment.loan.api.rest.controller.exception.LoanValidationException;
 import com.assesment.loan.domain.Loan;
 import com.assesment.loan.domain.LoanStatus;
 import com.assesment.loan.repository.LoanRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoanService  {
 
-    
+    @Autowired
     private LoanRepository repository;
 
     public LoanService() {
-    }
-    
-    public LoanService(LoanRepository repository) {
-      this.repository=repository;
     }
 
     public Loan createLoan(Loan loan) {
